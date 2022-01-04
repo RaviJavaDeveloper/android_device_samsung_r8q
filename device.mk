9ab27f599b5a3a19dd38cba8966e841fa733fa20
RaviJavaDeveloper <sukolaravi@gmail.com>
2022-01-04 19:29:19 +0530

    Edited oneplus 8 tree for samsung s20 fe : Booting properly

---
+++ device.mk
@@ -0,0 +1,53 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-aosp
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
+
+# Device init scripts
+PRODUCT_PACKAGES += \
+    fstab.qcom
+
+# API Level
+PRODUCT_SHIPPING_API_LEVEL := 29
+
+# Inherit from samsung sm8250-common
+$(call inherit-product, device/samsung/sm8250-common/common.mk)
+
+# Inherit from vendor blobs
+$(call inherit-product, vendor/samsung/r8q/r8q-vendor.mk)
